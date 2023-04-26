2774ca5092ca71edcb2960856c3ed310df573a77
ELIEL ALVES DOS SANTOS <eliel_a_santos@estudante.sesisenai.org.br>
2023-04-26 12:28:52 -0300

    começo do js

--- calculos.js
+++ calculos.js
@@ -1,37 +1,27 @@
-let parcial = document.getElementById('parcial')
-let total = document.getElementById('total')
-let vetor = document.getElementById('vetor')
-let valor_compra = document.getElementById('valor_compra')
+function calcularValorTotal() {
+    // Obter os valores informados pelo usuário
+    var qtdCachorroQuente = parseInt(document.getElementById("qtdCachorroQuente").value);
+    var qtdMistoQuente = parseInt(document.getElementById("qtdMistoQuente").value);
+    var qtdBauru = parseInt(document.getElementById("qtdBauru").value);
+    var qtdCheeseBurguer = parseInt(document.getElementById("qtdCheeseBurguer").value);
+    var qtdCheeseEgg = parseInt(document.getElementById("qtdCheeseEgg").value);
+    var qtdCheeseFrango = parseInt(document.getElementById("qtdCheeseFrango").value);
+    var qtdRefrigerante = parseInt(document.getElementById("qtdRefrigerante").value);
+    var qtdSucoFrutas = parseInt(document.getElementById("qtdSucoFrutas").value);
+    var qtdCafeMedio = parseInt(document.getElementById("qtdCafeMedio").value);
+    var qtdCafeLeite = parseInt(document.getElementById("qtdCafeLeite").value);
 
-let tipo_produto = []
-let quantidade = []
-let preco_produtos = []
+    // Calcular o valor total do pedido
+    var valorTotal = (qtdCachorroQuente * 7.9) + (qtdMistoQuente * 5.75) + (qtdBauru * 11.35) +
+                     (qtdCheeseBurguer * 14.6) + (qtdCheeseEgg * 15.4) + (qtdCheeseFrango * 12.9) +
+                     (qtdRefrigerante * 4.9) + (qtdSucoFrutas * 4.3) + (qtdCafeMedio * 3.5) +
+                     (qtdCafeLeite * 4.2);
 
-
-parcial.addEventListener('click', ()=>{
-    let produto = document.getElementById('produto').value
-    let qtde_produtos = document.getElementById('qtde_produtos').value
-    let preco_produto = document.getElementById('preco_produto').value
-
-    tipo_produto.push(produto)
-    quantidade.push(qtde_produtos)
-    preco_produtos.push(preco_produto)
-
-    // console.log(tipo_produto)
-    // console.log(quantidade)
-    // console.log(preco_produtos)
-    
-    let soma = 0.0
-    for(i=0;i<preco_produtos.length;i++){
-        soma = soma + (quantidade[i]*preco_produtos[i])
+    // Aplicar o desconto de 5%, caso o valor total seja superior a R$100,00
+    if (valorTotal > 100) {
+        valorTotal = valorTotal * 0.95;
     }
-   
-    vetor.innerHTML = ''
-    for(i=0;i<preco_produtos.length;i++){
-        vetor.innerHTML += 'Produto = ' + tipo_produto[i] + '&emsp;'
-        vetor.innerHTML += 'Quantidade = ' + quantidade[i] + '&emsp;'
-        vetor.innerHTML += 'Preço produto = R$ ' + preco_produtos[i] + '<br>'
-    }
-    
 
-})
+    // Exibir o valor total na tela
+    document.getElementById("valorTotal").innerHTML = "Valor total do pedido: R$" + valorTotal.toFixed(2);
+}
